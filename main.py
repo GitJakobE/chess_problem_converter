cb36c4b25ec5a0c187d51ad7e1da867d6848b1fe
Jakob Enemark <extjae@trifork.com>
2024-01-26 14:12:56 +0100

    rewrite, board found in most cases

--- main.py
+++ main.py
@@ -1,74 +1,37 @@
-import math
 import cv2 as cv
 import sys
-import numpy as np
-import json
-import matplotlib.pyplot as plt
 from util import Util
+from scipy.ndimage import rotate
 
 source_image = "data\Toft-00-0252.png" if len(sys.argv) < 2 else sys.argv[1]
-debug = True
 
 
 # Main function
 def main():
-    # import a pdf and convert its content to pngs
-    # Util.convert_pdf_to_pngs(r"D:\Skakproblemopgaver\frasoren\chess\chess\data\data\Toft-00\Toft-00-0018.pdf")
-
     # import, convert to grayscale and binarize
+    line_profile_width = 300
+    gaussian_sigma = 7
+    approx_board = 500
     image = cv.imread(source_image)
-    blue_channel = image[:, :, 2]
-    red_channel = image[:, :, 0]
-    green_channel = image[:, :, 1]
-    if debug == True:
-        cv.imwrite('blue_image.png', blue_channel)
-        cv.imwrite('red_image.png', red_channel)
-        cv.imwrite('green_image.png', green_channel)
 
-    blue_img = np.zeros(image.shape)
-    blue_img[:, :, 0] = blue_channel
-    image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    cv.imwrite('out/image_org.png', image)
 
-    retval, image_gray = cv.threshold(blue_channel, 128, 256, cv.THRESH_BINARY)
-    if (debug == True):
-        cv.imwrite('grayimage.png', image_gray)
-    # fig, ax = plt.subplots()
-    # im = ax.imshow(image)
-    # # plt.show()
-    cv.imshow('image', blue_img)
-    # Determine lines using hough transform
-    horizontal_lines, vertical_lines = Util.find_horizontal_vertical_lines(image_gray)
-    # debug: draw_lines(image_gray, horizontal_lines);
+    right_tilt_angle, left_tilt_angle = Util.find_edge(image, line_profile_width, gaussian_sigma, 10)
+    rotated_image = rotate(image, (right_tilt_angle + left_tilt_angle) / 2, reshape=False)
 
-    # Determine angle of lines and use to derotate image
-    angle = Util.find_average_rotation(horizontal_lines, vertical_lines)
-    angle_deg = 180.0 * angle / math.pi
-    de_rotated_image_gray = Util.rotate_image(image_gray, angle_deg)
+    left_edge, right_edge = Util.find_board_edges(rotated_image, line_profile_width, gaussian_sigma, approx_board)
+    image = Util.remove_black_writing(image)
+    cv.imwrite(f'out/full_rotated.png', image)
 
-    # Locate the positions of the 'templates'
-    template_locations = Util.locate_templates(de_rotated_image_gray)
+    board_width = right_edge - left_edge
+    tile_width = board_width // 8
+    top_line = Util.find_top_line(image, left_edge, right_edge, tile_width)
 
-    # Locate lines again on derotated image to find bounding box
-    horizontal_lines, vertical_lines = Util.find_horizontal_vertical_lines(de_rotated_image_gray)
-    bounding_box = Util.find_bounding_box(horizontal_lines, vertical_lines)
+    board = rotated_image[top_line:top_line + board_width, left_edge:right_edge]
 
-    bb_image_gray = image
-    cv.rectangle(bb_image_gray, (bounding_box[0], bounding_box[1]), (bounding_box[2], bounding_box[3]), (0, 0, 255), 2)
-    # cv.imshow('image', bb_image_gray)
-    cv.imwrite('output.png', de_rotated_image_gray)
-    fig, ax = plt.subplots()
-    im = ax.imshow(bb_image_gray)
-    plt.show()
-    # Setup the 8x8 grid and find out which cells are occupied by which templates
-    grid = Util.setup_grid()
-    grid = Util.fill_templates_in_grid(grid, bounding_box, template_locations)
-    # Output grid as JSON
-    print(json.dumps(grid))
+    Util.print_board(board)
 
-    # Draw output
-    Util.draw_locations(de_rotated_image_gray, template_locations)
-    cv.imwrite('output.png', de_rotated_image_gray)
-
+    Util.print_pieces(board)
 
 # Run the program
 main()
--- util/util.py
+++ util/util.py
@@ -2,13 +2,161 @@ import cv2 as cv
 import numpy as np
 from math import pi
 from pdf2image import convert_from_path
+from scipy.ndimage import gaussian_filter, rotate
 
 
 class Util:
     @staticmethod
+    def print_board(board):
+        _, board_width, _ = board.shape
+        tile_width = board_width//8
+        tol = 2
+
+        cv.imwrite(f'out/board/board.png', board)
+        for y in range(8):
+            for n, letter in enumerate(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']):
+                tile = board[board_width + tol - (y + 1) * tile_width:board_width - tol - y * tile_width,
+                       tile_width * n + tol:tile_width * (1 + n) - tol]
+                cv.imwrite(f'out/board/{letter}{y + 1}.png', tile)
+
+    @staticmethod
+    def print_board(board):
+        _, board_width, _ = board.shape
+        tile_width = board_width//8
+        tol = 2
+
+        cv.imwrite(f'out/board/board.png', board)
+        for y in range(8):
+            for n, letter in enumerate(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']):
+                tile = board[board_width + tol - (y + 1) * tile_width:board_width - tol - y * tile_width,
+                       tile_width * n + tol:tile_width * (1 + n) - tol]
+                cv.imwrite(f'out/board/{letter}{y + 1}.png', tile)
+
+    @staticmethod
+    def find_top_line(image, left_edge, right_edge, tile_width):
+        height, width, _ = image.shape
+
+        # find the middel of a square/tile
+        min_dif = 1000
+        best_i = 0
+        tol = 3
+
+        for i in range(tile_width):
+            line_profile = image[(height - tile_width)//2 + i:(height + tile_width)//2 + i,
+                           left_edge:right_edge]
+            tiles = []
+            for x in range(8):
+                tiles.append(line_profile[:, x * tile_width + tol:(x + 1) * tile_width - tol])
+            min_cross = np.min([np.std(gaussian_filter(tile, sigma=9)) for tile in tiles])
+
+            if min_cross < min_dif:
+                best_i = i
+                min_dif = min_cross
+        y_line = height//2 + best_i
+        cv.imwrite(f'out/line_profile.png', image[y_line - tile_width // 2 + 5:y_line - 5 + tile_width // 2,
+        left_edge:right_edge])
+        top_reached = False
+
+        top_line = 0
+        while not top_reached:
+            y_line = y_line - tile_width
+            line_profile = image[y_line - tile_width//2 + 5:y_line - 5 + tile_width//2,
+                           left_edge:right_edge]
+            tiles = []
+            for x in range(8):
+                tiles.append(line_profile[:, x * tile_width + tol + 5:(x + 1) * tile_width - tol - 5])
+            if np.min([np.std(tiles[i]) for i in range(8)]) * 2 > np.std(line_profile):
+                top_reached = True
+                top_line = y_line + tile_width // 2
+
+        return top_line
+
+    @staticmethod
+    def remove_black_writing(image):
+        height, width, _ = image.shape
+        th = 150
+        # remove black:
+        for x in range(width):
+            for y in range(height):
+                if np.all(image[y][x] < th):
+                    for color in range(3):
+                        image[y][x][color] = 230
+        return image
+    @staticmethod
+    def find_board_edges(image, line_profile_width, gaussian_sigma, approx_board):
+        height, width, _ = image.shape
+        side_cut = 5
+
+        flattened_profile = np.mean(
+            image[(height - line_profile_width) // 2:(height + line_profile_width) // 2,
+            side_cut:width - side_cut], axis=0)
+
+        smoothed_profile = gaussian_filter(flattened_profile, sigma=gaussian_sigma)
+
+        max_div = 0
+        max_tol = 10
+        pos_left = 0
+        pos_right = width - 1
+
+        # Calculate the derivative
+        for x in range(max_tol, len(smoothed_profile) - max_tol - approx_board):
+            for tol in range(-max_tol, max_tol):
+                derivative = np.mean(
+                    smoothed_profile[x] - smoothed_profile[x + 1] + smoothed_profile[x + approx_board + tol] -
+                    smoothed_profile[
+                        x + approx_board - 1 + tol])
+                if derivative > max_div:
+                    max_div = derivative
+                    pos_left = x
+                    pos_right = x + approx_board + tol
+        pos_left = pos_left + int(gaussian_sigma / 2) + side_cut
+        pos_right = pos_right + int(gaussian_sigma / 2) + side_cut
+        return pos_left, pos_right
+
+    @staticmethod
+    def find_edge(image, line_profile_width=300, gaussian_sigma=5, tilt_range=10):
+        height, width, _ = image.shape
+        h_line_profile_width = line_profile_width // 2
+        edge_tol = 10
+        # Select the area for the line profile based on the edge we are processing
+        profile_slice = image[height // 2 - h_line_profile_width:height // 2 + h_line_profile_width,
+                        edge_tol:width - edge_tol]
+
+        # Calculate the average color across the vertical axis to get the profile
+        profile = np.mean(profile_slice, axis=2)
+
+        # Initialize variables to find the maximum derivative and its corresponding angle
+        right_max_derivative_value = left_max_derivative_value = 0
+        right_max_derivative_angle = left_max_derivative_angle = 0
+        # Rotate the profile within the specified range and find the angle with the highest first derivative
+        for angle in np.arange(-tilt_range, tilt_range + 1):
+            # Rotate the profile
+            rotated_profile = rotate(profile, angle, reshape=False)
+
+            flattened_profile = np.median(rotated_profile, axis=0)
+            # Apply Gaussian filter
+            smoothed_profile = gaussian_filter(flattened_profile, sigma=gaussian_sigma)
+
+            # Calculate the derivative
+            derivative = np.gradient(smoothed_profile, axis=0)
+            left_max_derivative = np.min(derivative[:150])
+            right_max_derivative = np.max(derivative[150:])
+
+            # If the new derivative is greater than the max, update the max derivative and angle
+            cv.imwrite(f'../out/test{angle}.png', rotated_profile)
+            if left_max_derivative > left_max_derivative_value:
+                left_max_derivative_value = left_max_derivative
+                left_max_derivative_angle = angle
+            if right_max_derivative > right_max_derivative_value:
+                right_max_derivative_value = right_max_derivative
+                right_max_derivative_angle = angle
+
+        return right_max_derivative_angle, left_max_derivative_angle
+
+    @staticmethod
     def locate_templates(image_gray):
         """ Locate points where the templates appear in image_gray"""
-        templates = ['data\wpawn.png', 'data\whorse.png', 'data\bpawn.png', 'data\bhorse.png']
+        templates = [r'data\wpawn.png', r'data\whorse.png', r'data\bpawn.png', r'data\bhorse.png']
         locations = []
         for template_name in templates:
             template = cv.imread(template_name)
@@ -75,14 +223,14 @@ class Util:
         y1 = int(y0 + 10000 * a)
         x2 = int(x0 - 10000 * (-b))
         y2 = int(y0 - 10000 * a)
-        return x0, y0, x1, y1, x2, y2
+        return x1, y1, x2, y2
 
     @staticmethod
     def draw_lines(target_image, lines):
         """Draw hough lines on targetImage"""
         for i in range(len(lines)):
             for rho, theta in lines[i]:
-                x0, y0, x1, y1, x2, y2 = Util.points_for_line(rho, theta)
+                x1, y1, x2, y2 = Util.points_for_line(rho, theta)
 
                 cv.line(target_image, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
@@ -115,11 +263,11 @@ class Util:
         min_y = 1024
         max_y = 0
         for h_l in horizontal_lines:
-            _, _, _, y, _, _ = Util.points_for_line(h_l[0][0], h_l[0][1])
+            _, y, _, _ = Util.points_for_line(h_l[0][0], h_l[0][1])
             min_y = min(min_y, y)
             max_y = max(max_y, y)
         for v_l in vertical_lines:
-            _, _, x, _, _, _ = Util.points_for_line(v_l[0][0], v_l[0][1])
+            x, _, _, _ = Util.points_for_line(v_l[0][0], v_l[0][1])
             min_x = min(min_x, x)
             max_x = max(max_x, x)
         return min_x, min_y, max_x, max_y
